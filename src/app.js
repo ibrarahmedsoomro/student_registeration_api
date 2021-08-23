b38e0dbc6498bd38fce37a75e7200efa0ac1259b
Ibrar Ahmed <50750853+ibrarsoomro@users.noreply.github.com>
2021-08-23 18:50:20 +0500

    asyn await
    used asyn await

--- src/app.js
+++ src/app.js
@@ -9,15 +9,29 @@ app.get("/", (req, res)=>{
   res.send("Hello Jhal Dubb")    
 })
 
-app.post("/students", (req, res)=>{
-    console.log(req.body)
-    const user = new Student(req.body);
+//app.post("/students",(req, res)=>{
+//     console.log(req.body)
     
-     user.save().then( ()=>{
-         res.status(201).send(user)
-     }).catch( (err)=> {
-         res.status(400).send(err)
-     })
+    
+//      user.save().then( ()=>{
+//          res.status(201).send(user)
+//      }).catch( (err)=> {
+//          res.status(400).send(err)
+//      })
+// })
+
+app.post("/students",  async(req,res)=>{
+
+    try {
+        const user = new Student(req.body);
+        const Createuser = await user.save()
+        res.status(201).send(Createuser);
+    }
+    catch(err){
+        res.status(404).send(err);
+    }
+  
+
 })
 app.listen(port, ()=>{
     console.log(`Server is Running http://localhost:${port}`)
